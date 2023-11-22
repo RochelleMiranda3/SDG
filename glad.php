f72e00560a60e96758df1c65cbf0664859f6150f
Gladys C. Lalucis <143146750+lalucisgladys@users.noreply.github.com>
2023-11-22 18:41:07 +0800

    Create glad.php

---
+++ glad.php
@@ -0,0 +1,281 @@
+<!--This div is for research 5.9.1-->
+                    <div class=" w-100 h-auto" id="research591" style="display: none;">
+                        <h2 class="text-center pb-4">Capacity Development Sessions</h2>
+                        <form action="" method="post">
+
+                            <div class="input-group mb-3">
+                                <span class="input-group-text"><i class=""></i></span>
+                                <input type="text" class="form-control" placeholder="Total number of development sessions " name="title">
+                            </div>
+                    
+                            <div class="input-group mb-3">
+                                <span class="input-group-text"><i class=""></i></span>
+                                <input type="text" class="form-control" placeholder="Total number of development sessions" name="author">
+                            </div>
+
+                            <div class="d-grid">
+                                <button type = "submit" class= "btn btn-success mb-3" name= "submit">Add
+                                    Research</button> 
+                            </div>
+                            </form>
+
+                    <!--This div is for research 5.9.2-->
+                    <div class=" w-100 h-auto" id="research 592" style="display: none;">
+                    <h2 class="text-center pb-4">GAD Observances</h2>
+                        <form action="" method="post">
+
+                            <div class="input-group mb-3">
+                                <span class="input-group-text"><i class=""></i></span>
+                                <input type="text" class="form-control" placeholder="Total number of GAD observances " name="title">
+                            </div>
+                    
+                            <div class="input-group mb-3">
+                                <span class="input-group-text"><i class=""></i></span>
+                                <input type="text" class="form-control" placeholder="Total number of GAD observances" name="author">
+                            </div>
+
+                            <div class="d-grid">
+                                <button type = "submit" class= "btn btn-success mb-3" name= "submit">Add
+                                    Research</button> 
+                            </div>
+                            </form>
+
+                    <!--This div is for research 5.9.3-->
+                    <div class=" w-100 h-auto" id="research 593" style="display: none;">
+                    <h2 class="text-center pb-4">GAD IEC Materials</h2>
+                        <form action="" method="post">
+
+                            <div class="input-group mb-3">
+                                <span class="input-group-text"><i class=""></i></span>
+                                <input type="text" class="form-control" placeholder="Total number of GAD IEC materials " name="title">
+                            </div>
+                    
+                            <div class="input-group mb-3">
+                                <span class="input-group-text"><i class=""></i></span>
+                                <input type="text" class="form-control" placeholder="Total number of GAD IEC materials" name="author">
+                            </div>
+
+                            <div class="d-grid">
+                                <button type = "submit" class= "btn btn-success mb-3" name= "submit">Add
+                                    Research</button> 
+                            </div>
+                            </form>
+                    
+                    <!--This div is for research 5.9.4-->
+                    <div class=" w-100 h-auto" id="research 594" style="display: none;">
+                    <h2 class="text-center pb-4">Library Resources</h2>
+                        <form action="" method="post">
+
+                            <div class="input-group mb-3">
+                                <span class="input-group-text"><i class=""></i></span>
+                                <input type="text" class="form-control" placeholder="Total number of library resources" name="title">
+                            </div>
+                    
+                            <div class="input-group mb-3">
+                                <span class="input-group-text"><i class=""></i></span>
+                                <input type="text" class="form-control" placeholder="Total number of library resources" name="author">
+                            </div>
+
+                            <div class="d-grid">
+                                <button type = "submit" class= "btn btn-success mb-3" name= "submit">Add
+                                    Research</button> 
+                            </div>
+                            </form>
+                    
+                    <!--This div is for research 5.10.1-->
+                    <div class=" w-100 h-auto" id="research 5101" style="display: none;">
+                    <h2 class="text-center pb-4">Courses integrated with gender perspective</h2>
+                        <form action="" method="post">
+
+                            <div class="input-group mb-3">
+                                <span class="input-group-text"><i class=""></i></span>
+                                <input type="text" class="form-control" placeholder="Total number of courses integrated with gender perspective " name="title">
+                            </div>
+                    
+                            <div class="input-group mb-3">
+                                <span class="input-group-text"><i class=""></i></span>
+                                <input type="text" class="form-control" placeholder="Total number of courses integrated with gender perspective" name="author">
+                            </div>
+
+                            <div class="d-grid">
+                                <button type = "submit" class= "btn btn-success mb-3" name= "submit">Add
+                                    Research</button> 
+                            </div>
+                            </form>
+
+
+                    <!--This div is for research 5.11.1-->
+                    <div class=" w-100 h-auto" id="research 5111" style="display: none;">
+                    <h2 class="text-center pb-4">Extension PPAs integrated with gender perspective</h2>
+                        <form action="" method="post">
+
+                            <div class="input-group mb-3">
+                                <span class="input-group-text"><i class=""></i></span>
+                                <input type="text" class="form-control" placeholder="Total number of extension PPAs integrated with gender perspective " name="title">
+                            </div>
+                    
+                            <div class="input-group mb-3">
+                                <span class="input-group-text"><i class=""></i></span>
+                                <input type="text" class="form-control" placeholder="Total number of extension PPAs integrated with gender perspective" name="author">
+                            </div>
+
+                            <div class="d-grid">
+                                <button type = "submit" class= "btn btn-success mb-3" name= "submit">Add
+                                    Research</button> 
+                            </div>
+                            </form>
+
+
+
+
+    //button
+    var btn_res511 = document.getElementById("res511");
+    var btn_res521 = document.getElementById("res521");
+    
+    //div
+    var div_res511 = document.getElementById("research511");
+    var div_res521 = document.getElementById("research521");
+
+    //button
+    var btn_res531 = document.getElementById("res531");
+    var btn_res532 = document.getElementById("res532");
+
+    //div
+    var div_res531 = document.getElementById("research531");
+    var div_res532 = document.getElementById("research532");
+
+    //button
+    var btn_res533 = document.getElementById("res533");
+    var btn_res534 = document.getElementById("res534");
+
+    //div
+    var div_res533 = document.getElementById("research533");
+    var div_res534 = document.getElementById("research534");
+
+    //button
+    var btn_res535 = document.getElementById("res535");
+    var btn_res541 = document.getElementById("res541");
+
+    //div
+    var div_res535 = document.getElementById("research535");
+    var div_res541 = document.getElementById("research541");
+
+    //button
+    var btn_res551 = document.getElementById("res551");
+    var btn_res552 = document.getElementById("res552");
+
+    //div
+    var div_res551 = document.getElementById("research551");
+    var div_res552 = document.getElementById("research552");
+
+    //button
+    var btn_res553 = document.getElementById("res553");
+    var btn_res554 = document.getElementById("res554");
+
+    //div
+    var div_res553 = document.getElementById("research553");
+    var div_res554 = document.getElementById("research554");
+
+    //button
+    var btn_res561 = document.getElementById("res561");
+    var btn_res562 = document.getElementById("res562");
+
+    //div
+    var div_res561 = document.getElementById("research561");
+    var div_res562 = document.getElementById("research562");
+
+    //button
+    var btn_res563 = document.getElementById("res563");
+    var btn_res564 = document.getElementById("res564");
+
+    //div
+    var div_res563 = document.getElementById("research563");
+    var div_res564 = document.getElementById("research564");
+
+    //button
+    var btn_res565 = document.getElementById("res565");
+    var btn_res566 = document.getElementById("res566");
+
+    //div
+    var div_res565 = document.getElementById("research565");
+    var div_res566 = document.getElementById("research566");
+
+    //button
+    var btn_res567 = document.getElementById("res567");
+    var btn_res568 = document.getElementById("res568");
+
+    //div
+    var div_res567 = document.getElementById("research567");
+    var div_res568 = document.getElementById("research568");
+
+    //button
+    var btn_res569 = document.getElementById("res569");
+    var btn_res5610 = document.getElementById("res5610");
+
+    //div
+    var div_res569 = document.getElementById("research569");
+    var div_res5610 = document.getElementById("research5610");
+
+    //button
+    var btn_res571 = document.getElementById("res571");
+    var btn_res572 = document.getElementById("res572");   
+
+    //div
+    var div_res571 = document.getElementById("research571");
+    var div_res572 = document.getElementById("research572");
+
+    //button
+    var btn_res573 = document.getElementById("res573");
+    var btn_res574 = document.getElementById("res574");
+
+    //div
+    var div_res573 = document.getElementById("research573");
+    var div_res574 = document.getElementById("research574");
+
+    //button
+    var btn_res575 = document.getElementById("res575");
+    var btn_res576 = document.getElementById("res576");
+
+    //div
+    var div_res575 = document.getElementById("research575");
+    var div_res576 = document.getElementById("research576");
+
+    //button
+    var btn_res5761 = document.getElementById("res5761");
+    var btn_res5762 = document.getElementById("res5762");
+
+    //div
+    var div_res5761 = document.getElementById("research5761");
+    var div_res5762 = document.getElementById("research5762");
+
+    //button
+    var btn_res581 = document.getElementById("res581");
+    var btn_res582 = document.getElementById("res582");
+
+    //div
+    var div_res581 = document.getElementById("research581");
+    var div_res582 = document.getElementById("research582");
+
+    //button
+    var btn_res591 = document.getElementById("res591");
+    var btn_res592 = document.getElementById("res592");
+
+    //div
+    var div_res591 = document.getElementById("research591");
+    var div_res592 = document.getElementById("research592");
+
+    //button
+    var btn_res593 = document.getElementById("res593");
+    var btn_res594 = document.getElementById("res594");
+
+     //div
+    var div_res593 = document.getElementById("research593");
+    var div_res594 = document.getElementById("research594");
+
+    //button
+    var btn_res5101 = document.getElementById("res5101");
+    var btn_res5111 = document.getElementById("res5111");
+
+    //div
+    var div_res5101 = document.getElementById("research5101");
+    var div_res5111 = document.getElementById("research5111");
