84ae0feca042b6ed2c50e0a00570dfc86a0a41fb
Gladys C. Lalucis <143146750+lalucisgladys@users.noreply.github.com>
2023-11-28 21:49:17 +0800

    Create insert569.php

---
+++ insert569.php
@@ -0,0 +1,27 @@
+<?php
+include '../../../includes/db.php';
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+    $ID = $_POST['ID'];
+    $no_policy_protecting = $_POST['no_policy_protecting'];
+    $no_ppa_implemented = $_POST['no_ppa_implemented'];
+    $title_ppa = $_POST['title_ppa'];
+    $description_ppa = $_POST['description_ppa'];
+    $total_cost = $_POST['total_cost'];
+    $fund_source = $_POST['fund_source'];
+
+    $query = "INSERT INTO `research5.6.9` (ID, no_policy_protecting , no_ppa_implemented, title_ppa, description_ppa, total_cost, fund_source  ) VALUES ('$ID', '$no_policy_protecting', '$no_ppa_implemented', '$title_ppa', '$description_ppa', '$total_cost', '$fund_source')";
+    $insert = mysqli_query($conn,$query);
+
+    if($insert){
+        header("Location:../../../admin/sdg5/formsdg5.php?message=Insert Successful");
+    }else if(!$insert){
+        die("Query failed. " . mysqli_error($conn));
+    }
+}
+else {
+    echo "Invalid request method";
+}
+
+?>
